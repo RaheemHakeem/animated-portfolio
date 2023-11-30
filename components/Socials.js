f4025a5bda42993c58bbcb2890e8db5945a5e45e
Rav <raheemhakeem25@gmail.com>
2023-11-30 23:55:23 +0100

    working on about page

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,29 @@
+// linkss
+import Link from "next/link";
+//icons
+import {
+  RiYoutubeLine,
+  RiInstagramLine,
+  RiFacebookLine,
+  RiDribbleLine,
+  RiBehanceLine,
+  RiPinterestLine,
+} from "react-icons/ri";
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className="flex items-center gap-x-5 text-lg">
+      <Link href={""} className="hover:text-accent transition-all duration-300">
+        <RiYoutubeLine />
+      </Link>
+      <Link href={""} className="hover:text-accent transition-all duration-300">
+        <RiInstagramLine />
+      </Link>
+      <Link href={""} className="hover:text-accent transition-all duration-300">
+        <RiFacebookLine />
+      </Link>
+    </div>
+  );
 };
 
 export default Socials;
